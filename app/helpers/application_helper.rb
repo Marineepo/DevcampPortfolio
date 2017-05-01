9d17b419ad754eba49ef28bc643265d85c9218e1
Jordan Hudgens <jordan@devcamp.com>
2017-05-01 18:14:14 +0000

    intergerated gem into rails app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    NicklausViewTool::Renderer.copyright 'Benjamin Nicklaus', 'All right reserved'
+  end
 end
